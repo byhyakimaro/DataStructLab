952307aa97f7ec3682cd9a9c65e5cba9ce76924c
hyakimaro <moderrevolution090217@gmail.com>
2025-05-18 20:20:05 -0300

    create for looping

--- binary_tree/src/main.rs
+++ binary_tree/src/main.rs
@@ -2,12 +2,16 @@ use data_struct_lab::Tree;
 
 fn main() {
     let mut tree = Tree::new();
- 
+
     tree.insert(42);
     tree.insert(21);
     tree.insert(84);
     tree.insert(5);
     tree.insert(63);
+
+    for elem in [1, 2, 3, 4] {
+        tree.insert(elem);
+    }
 
     println!("{}", tree);
 }
